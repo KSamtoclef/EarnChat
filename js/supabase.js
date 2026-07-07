cc9fbdabc36e9541fc70b6cc5a9715b6958ac264
KSamtoclef <samtoclef@gmail.com>
2026-07-07 09:12:13 +0100

    Delete js directory

--- js/supabase.js
+++
@@ -1,9 +0,0 @@
-const SUPABASE_URL = "https://dtjxcgzpwemdgdeinkcl.supabase.co";
-
-const SUPABASE_ANON_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0anhjZ3pwd2VtZGdkZWlua2NsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MDg0ODQsImV4cCI6MjA5MzQ4NDQ4NH0.kGjtOZfK7onzr-3FVMuSljiJ3emllxtGdepxrFVUPPM";
-
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
